329171e437503c568645f77a72df143a8d6597d0
Kaleb Barone <52588235+kaleblob@users.noreply.github.com>
2024-08-22 17:35:53 +1000

    Add files via upload

---
+++ wh_calcs.py
@@ -0,0 +1,389 @@
+from random import randint
+from math import ceil
+import numpy as np
+import matplotlib.pyplot as plt 
+from copy import deepcopy
+
+from wh_profiles import *
+
+
+
+# ~~~ ignoring these for now ~~~
+# TODO: add options for spillover to characters in attached units?  best way is to just "slow roll" everything, forget
+# numpy just use a for loop for number of attacks.
+# TODO: sequence of attacks from multiple units?  where the conditions of the attack might change, e.g. the first unit
+# gets the target below half strength, then the 2nd unit has a rule that benefits from that.  similarly for chewing 
+# through a bodyguard unit, then the character gets a different profile.  or precision potentially killing the character 
+# first, which changes the defensive characteristics of the bodyguard unit.
+# TODO: for CP reroll, determine where best to reroll by getting statistics from different dice rolls and maximising
+# either probability of destroying unit or avg damage (you choose).  E.g. is it better to reroll number of attacks when 
+# you rolled a 3, or is it better to save until a damage roll?
+# ~~~ ignoring these for now ~~~
+
+
+def make_attacks(
+    # required parameters
+    strength,
+    toughness,
+    armour_save,
+    unit_wounds_profile,
+
+    # attacker parameters
+    num_weapons = 1,
+    attacks = 0,
+    attacks_d3 = 0,
+    attacks_d6 = 0,
+    reroll_attacks = False,
+    blast = False,
+
+    ballistic_skill = 7,
+    torrent = False,
+    crit_hit_roll = 6,
+    hit_reroll_1 = False,
+    hit_reroll = False,
+    hit_reroll_no_crit = False,
+    lethal_hits = False,
+    sustained_hits = 0,
+
+    crit_wound_roll = 6,
+    wound_reroll_1 = False,
+    wound_reroll = False,
+    wound_reroll_no_crit = False,
+    dev_wounds = False,
+
+    ap = 0,
+    ignores_cover = False,
+
+    damage = 0,
+    damage_d3 = 0,
+    damage_d6 = 0,
+    damage_reroll = False,
+    cp_reroll = False,          # TODO:  a reroll used only once throughout this attack, prioritises damage > wound > hit
+
+    # target parameters
+    invuln_save = 7,
+    cover = False,
+    halve_damage = False,
+    feel_no_pain = 7,
+
+    # mods
+    attacks_mod= 0,
+    hit_mod = 0,
+    strength_mod = 0,
+    toughness_mod = 0,
+    wound_mod = 0,
+    ap_mod = 0,
+    armour_mod = 0,
+    damage_mod = 0,
+
+    # statistics
+    statistics_dict = {
+        'total_damage': 0,
+        'damage_spillover': 0
+    }
+):
+    """
+    Makes attacks against a unit with 1 weapon profile.  Resolve multiple attacks by stringing this function together and
+    updating the target profile.  Modify the calculation by adding an 'extra_effects' dictionary.
+    E.g.
+    target_profile['unit_wounds_profile'] = make_attacks(num_weapons=4, **attack_profile_1, **target_profile)
+    target_profile['unit_wounds_profile'] = make_attacks(**{**attack_profile_2, **extra_effects}, **target_profile)
+
+    Returns: 
+        List:   each entry gives the remaining wounds of each model in the target unit
+    """
+
+
+    # get which model in the unit to target
+    for model_id in range(len(unit_wounds_profile)):
+        if unit_wounds_profile[model_id] > 0:
+            current_wounds = unit_wounds_profile[model_id]
+            break
+    models_in_unit = len(unit_wounds_profile) - model_id
+    if model_id >= len(unit_wounds_profile):
+        raise ValueError("Unit is already dead lmao")
+
+    # TODO: initialise some attack statistics
+    statistics_dict['damage_spillover'] = statistics_dict.get('damage_spillover', 0)
+    # overkill = target_profile.get('overkill', 0)
+
+
+
+    ### hit rolls ###
+    
+    # get number of attacks
+    num_attacks = (attacks+attacks_mod) * num_weapons
+    rolls = np.random.randint(1, 7, attacks_d6*num_weapons)
+    if reroll_attacks:
+        # currently assumes rerolling attacks if less than average (3.5)
+        for i, roll in enumerate(rolls):
+            if roll < 3.5:
+                rolls[i] = randint(1,6)
+    num_attacks += rolls.sum()
+    rolls = np.random.randint(1,4, attacks_d3*num_weapons)
+    if reroll_attacks:
+        for i, roll in enumerate(rolls):
+            if roll < 2:
+                rolls[i] = randint(1,3)
+    num_attacks += rolls.sum()
+    if blast:
+        num_attacks += models_in_unit//5*num_weapons
+
+
+    # get hits and crit hits
+    if torrent:
+        hits = num_attacks
+        crit_hits = 0
+    else:
+        # roll needed to hit
+        hit_roll = ballistic_skill-hit_mod
+        if hit_roll < 2:
+            hit_roll = 2
+        
+        # make hit rolls
+        rolls = np.random.randint(1, 7, num_attacks)
+        if hit_reroll_no_crit:
+            rolls[rolls<crit_hit_roll] = np.random.randint(1, 7, (rolls<crit_hit_roll).sum())
+        elif hit_reroll:
+            rolls[rolls<hit_roll] = np.random.randint(1, 7, (rolls<hit_roll).sum())
+        elif hit_reroll_1:
+            rolls[rolls==1] = np.random.randint(1, 7, (rolls==1).sum())
+
+        hits = np.logical_and(rolls>=hit_roll, rolls<crit_hit_roll).sum()
+        crit_hits = (rolls>=crit_hit_roll).sum()
+
+
+
+    ### wound rolls ###
+
+    # roll needed to wound
+    str_tough_ratio = (strength+strength_mod)/(toughness+toughness_mod)
+    if str_tough_ratio <= 0.5:
+        wound_roll = 6
+    elif str_tough_ratio > 0.5 and str_tough_ratio < 1:
+        wound_roll = 5
+    elif str_tough_ratio == 1:
+        wound_roll = 4
+    elif str_tough_ratio > 1 and str_tough_ratio < 2:
+        wound_roll = 3
+    elif str_tough_ratio >= 2:
+        wound_roll = 2
+
+    wound_roll += wound_mod
+    if wound_roll < 2:
+        wound_roll = 2
+    
+    # number of rolls to make
+    rolls_to_make = hits
+    rolls_to_make += sustained_hits*crit_hits
+    if not lethal_hits:
+        rolls_to_make += crit_hits
+
+    # do wound rolls
+    rolls = np.random.randint(1, 7, rolls_to_make)
+    if wound_reroll_no_crit:
+        rolls[rolls<crit_wound_roll] = np.random.randint(1, 7, (rolls<crit_wound_roll).sum())
+    elif wound_reroll:
+        rolls[rolls<wound_roll] = np.random.randint(1, 7, (rolls<wound_roll).sum())
+    elif wound_reroll_1:
+        rolls[rolls==1] = np.random.randint(1, 7, (rolls==1).sum())
+    
+    # calculate number of wounds and crit wounds
+    wounds = np.logical_and(rolls>=wound_roll, rolls<crit_wound_roll).sum()
+    if lethal_hits:
+        wounds += crit_hits
+    crit_wounds = (rolls>=crit_wound_roll).sum()
+
+
+
+    ### armour + invuln saves ###
+
+    # armour save modifications
+    armour_save += armour_mod
+    if armour_save < 2:
+        armour_save = 2
+    ap += ap_mod
+    if ap < 0:
+        ap = 0
+
+    if cover and not ignores_cover:
+        if armour_save > 3:
+            armour_save -= 1
+        elif ap > 0:
+            ap -= 1
+    
+    # roll needed to save
+    save_roll = invuln_save if invuln_save < armour_save + ap else armour_save + ap
+    
+    # make save rolls
+    rolls_to_make = wounds
+    if not dev_wounds:
+        rolls_to_make += crit_wounds
+    rolls = np.random.randint(1, 7, rolls_to_make)
+
+    damage_instances = (rolls<save_roll).sum()
+    if dev_wounds:
+        damage_instances += crit_wounds
+
+
+
+    ### damage ###
+
+    # apply damage instances one by one to account for spillover
+    for dmg_instance in range(damage_instances):
+        # get wounds of targeted model
+        current_wounds = unit_wounds_profile[model_id]
+
+        # calculate damage
+        dmg = damage
+        dmg_d6 = damage_d6*randint(1,6)
+        if damage_reroll:
+            # current heuristic:  if no fnp, don't reroll if already killing.  With fnp, just reroll if below avg. 
+            if dmg_d6 < 3.5:
+                if feel_no_pain < 7:
+                    dmg_d6 = randint(1,6)
+                elif dmg + dmg_d6 < current_wounds:
+                    dmg_d6 = randint(1,6)
+        dmg += dmg_d6
+        dmg_d3 = damage_d3*randint(1,3)
+        if damage_reroll and dmg_d3 < 2:
+            dmg_d3 = damage_d3*randint(1,3)
+        
+        if halve_damage:
+            dmg = ceil(dmg/2)
+        dmg += damage_mod
+
+        # apply feel no pain
+        if feel_no_pain < 7:
+            rolls = np.random.randint(1, 7, dmg)
+            dmg -= (rolls>=feel_no_pain).sum()
+
+        # apply damage to model
+        if dmg >= current_wounds:
+            # if damage exceeds wounds, kill model and further damage is lost
+            statistics_dict['total_damage'] += current_wounds
+            unit_wounds_profile[model_id] = 0
+            model_id += 1
+
+            # this is literally just to record how much damage was lost due to model overkill
+            # which isn't even an important stat, I was just curious lmao
+            dmg -= current_wounds
+            if model_id >= len(unit_wounds_profile):
+                # unit has been wiped
+                current_wounds = 0
+                break
+            statistics_dict['damage_spillover'] += dmg
+
+        else:
+            # if damage does not exceed wounds, subtract damage from wounds
+            unit_wounds_profile[model_id] -= dmg
+            statistics_dict['total_damage'] += dmg
+    
+
+    return unit_wounds_profile
+
+
+
+
+
+
+LIST_OF_EXTRA_EFFECTS = {
+    'attacks_mod': 0,
+    'hit_mod': 0,
+    'strength_mod': 0,
+    'toughness_mod': 0,
+    'wound_mod': 0,
+    'ap_mod': 0,            # NOTE: +1 ap_mod improves ap, -1 ap_mod worsens ap
+    'armour_mod': 0,        # NOTE: +1 armour_mod worsens armour, -1 armour_mod improves armour
+    'damage_mod': 0,
+    'halve_damage': False,  # half damage applies before damage_mod
+
+    # apart from the above mods, you can override literally any other characteristic here like so
+    'lethal_hits': True,
+    'sustained_hits': 2,
+    'crit_wound_roll': 4,
+    'toughness': 6,
+}
+
+
+stinky = {
+    'toughness_mod': -1,
+    'armour_mod': +1
+}
+
+# NOTE: use **dict to unpack a dictionary like so:
+# extra_effects = {
+#     'crit_hit_roll': 5,
+#     'lethal_hits',
+#     **stinky,
+# }
+
+### NOTE: MODIFY THIS TO ADD EFFECTS TO YOUR ATTACK ###
+extra_effects = {
+    'attacks_mod': 1,       # from rapid fire
+    'hit_mod': 1,           # from take aim order
+    'lethal_hits': True,    # from standing stationary
+}
+
+# target = {
+#     'toughness': 8,
+#     'armour_save': 2,
+#     'unit_wounds_profile': [7, 7, 7],
+# }
+
+### NOTE: CHANGE THIS HERE TO SELECT TARGET ###
+target = target_profile_space_marines
+target_profile = deepcopy(target)
+
+
+# repeat scenario some large amount of times, get distribution of outcomes
+# NOTE: lower this number if it gets too slow when doing something with many different attacks
+n_trials = 20000
+damage_done = np.zeros(n_trials)
+models_killed = np.zeros(n_trials)
+max_wounds = 0
+for i in range(len(target_profile['unit_wounds_profile'])):
+    max_wounds += target_profile['unit_wounds_profile'][i]
+
+
+for i in range(n_trials):
+    target_profile = deepcopy(target)
+    statistics = {'total_damage': 0}
+
+    # make attacks
+    ### NOTE: ADD YOUR ATTACKS HERE ###
+    target_profile['unit_wounds_profile'] = make_attacks(
+        num_weapons=2, **{**guard_plasma_gun_supercharge, **target_profile, **extra_effects}, statistics_dict=statistics
+    )
+    target_profile['unit_wounds_profile'] = make_attacks(
+        num_weapons=2, **{**guard_plasma_pistol_supercharge, **target_profile, **{'hit_mod': 1, 'lethal_hits': True}}, statistics_dict=statistics
+    )
+    target_profile['unit_wounds_profile'] = make_attacks(
+        num_weapons=2, **{**guard_meltagun, **target_profile, **{'hit_mod': 1, 'lethal_hits': True}}, statistics_dict=statistics
+    )
+    target_profile['unit_wounds_profile'] = make_attacks(
+        num_weapons=2, **{**guard_grenade_krak, **target_profile, **{'hit_mod': 1, 'lethal_hits': True}}, statistics_dict=statistics
+    )
+    target_profile['unit_wounds_profile'] = make_attacks(
+        num_weapons=12, **{**guard_lasgun, **target_profile, **extra_effects}, statistics_dict=statistics
+    )
+
+    # store results
+    damage_done[i] = statistics['total_damage']
+    models_killed[i] = target_profile['unit_wounds_profile'].count(0)
+
+
+
+# plot histogram, print probability of killing unit
+n_kills = (models_killed==len(target_profile['unit_wounds_profile'])).sum()
+print(f'Probability of killing unit:  {n_kills/n_trials}')
+plt.hist(damage_done, bins=np.arange(max_wounds+2)-0.5, density=True, cumulative=False)
+plt.xlabel('total damage')
+plt.ylabel('probability')
+plt.show()
+plt.hist(models_killed, bins=np.arange(len(target_profile['unit_wounds_profile'])+2)-0.5, density=True, cumulative=False)
+plt.xlabel('models slain')
+plt.ylabel('probability')
+plt.show()
+
+
---
+++ wh_profiles.py
@@ -0,0 +1,444 @@
+'''
+Various profiles for weapons and targets in WH40K 10th ed
+'''
+
+
+
+'''
+Some notes:
+- All melee weapon profiles should have 'ignores_cover': True,
+- Use extra_effects for things like hit rerolls from oath of moment, or debuffs from death guard contagion
+- ANTI-X is implemented using 'crit_wound_roll': 4+, and simply assuming the target has the X-keyword
+'''
+
+
+
+# Here are weapon and target profiles containing all possible keywords.  Most keywords have default values of 0 or False.
+
+attack_profile_template = {
+    'attacks': 2,                   # NOTE:  use 'num_weapons' to attack multiple times without changing the weapon profile
+    'attacks_d3': 0,                # number of d3 dice in the number of attacks
+    'attacks_d6': 0,                # number of d6 dice in the number of attacks
+    'reroll_attacks': False,        # rerolls number of attacks (if number of attacks used d3 or d6)
+    'blast': False,
+
+    'torrent': False,
+    'ballistic_skill': 3,
+    'crit_hit_roll': 6,             # roll needed to crit
+    'hit_reroll_1': False,          # rerolls a 1
+    'hit_reroll': True,             # rerolls a miss
+    'hit_reroll_no_crit': False,    # rerolls a non-crit
+    'lethal_hits': False,
+    'sustained_hits': 0,            # the integer indicates the added attacks, NOTE: sus hits d3 is not yet implemented
+
+    'strength': 4,
+    'crit_wound_roll': 6,           # use this for any 'ANTI-X' profiles
+    'wound_reroll_1': False,
+    'wound_reroll': False,
+    'wound_reroll_no_crit': False,
+    'dev_wounds': False,
+
+    'ap': 3,
+    'ignores_cover': True,
+    'damage': 1,
+    'damage_d6': 1,                 # number of d6 dice in the damage
+    'damage_d3': 0,                 # number of d3 dice in the damage
+    'damage_reroll': False,
+}
+
+target_profile_template = {
+    'toughness': 5,
+    'armour_save': 2,               # NOTE:  armour, invuln, and fnp of 7 or more means there is no armour/invuln/fnp save
+    'cover': False,
+    'invuln_save': 7,
+    'halve_damage': False,          # always applied first and rounded up
+    'damage_mod': -1,               # damage_mod is always applied after halving damage
+    'feel_no_pain': 7,
+    # 'starting strength': 5,       # TODO: implement 'below_half_strength'.  but does it really matter though?  not unless there are multiple attacking units.
+    'unit_wounds_profile': [4, 4, 3, 3, 3],    # target will always be selected first to last (left to right)
+}
+
+
+# Attacks are often modified by abilities, stratagems, etc.  Use this to add new and/or overwrite other keywords.
+extra_effects_template = {
+    'attacks_mod': 0,               # each 'mod' keyword simply adds its value to its characteristic/roll
+    'hit_mod': 0,                   # this adds to the hit roll, but does not change the roll required to crit
+    'strength_mod': 0,
+    'toughness_mod': 0,
+    'wound_mod': 0,                 # this adds to the wound roll, but does not change the roll required to crit wound
+    'ap_mod': 0,                    # NOTE: +1 ap_mod improves ap, -1 ap_mod worsens ap
+    'armour_mod': 0,                # NOTE: +1 armour_mod worsens armour, -1 armour_mod improves armour
+    'damage_mod': 0,
+    'halve_damage': False,          # half damage applies before damage_mod
+}
+
+
+
+
+
+
+
+##### WEAPON PROFILES #####
+
+
+
+### Space Marines ##
+
+sm_bolt_rifle = {
+    'attacks': 2,
+    'ballistic_skill': 3,
+    'strength': 4,
+    'ap': 1,
+    'damage': 1,
+}
+sm_astartes_grenade_launcher ={
+    'attacks_d3': 1,
+    'blast': True,
+    'ballistic_skill': 3,
+    'strength': 4,
+    'ap': 0,
+    'damage': 1,
+}
+sm_ballistus_lascannon = {
+    'attacks': 2,
+    'ballistic_skill': 3,
+    'hit_reroll': True,         # assumes enemy is not below half strength
+    'strength': 12,
+    'ap': 3,
+    'damage': 1,
+    'damage_d6': 1,
+}
+sm_ballistus_missile_launcher_krak = {
+    'attacks': 2,
+    'ballistic_skill': 3,
+    'hit_reroll': True,         # assumes enemy is not below half strength
+    'strength': 9,
+    'ap': 3,
+    'damage_d6': 1
+}
+sm_plasma_incinerator_supercharge = {
+    'attacks': 2,
+    'ballistic_skill': 3,
+    'strength': 8,
+    'ap': 3,
+    'damage': 2,
+}
+sm_aggressors_power_fists = {
+    'attacks': 3,
+    'ballistic_skill': 3,
+    'strength': 8,
+    'ap': 2,
+    'ignore_cover': True,
+    'damage': 2,
+}
+sm_plasma_pistol = {
+    'attacks': 1,
+    'ballistic_skill': 3,
+    'strength': 7,
+    'ap': 2,
+    'damage': 1,
+}
+sm_plasma_pistol_charge = {
+    'attacks': 1,
+    'ballistic_skill': 3,
+    'strength': 8,
+    'ap': 3,
+    'damage': 2,
+}
+
+
+
+### Custodes ###
+
+guardian_spear = {
+    'attacks': 5,
+    'ballistic_skill': 2,
+    'strength': 7,
+    'wound_reroll_1': True,
+    'ap': 2,
+    'damage': 2,
+}
+blade_champion_victus = {
+    'attacks': 7,
+    'ballistic_skill': 2,
+    'dev_wounds': True,
+    'lethal_hits': True,
+    'sustained_hits': 1,
+    'crit_hit_roll': 5,
+    'strength': 7,
+    'wound_reroll': False,
+    'ap': 3,
+    'damage': 4,
+}
+
+
+
+### Deathguard ###
+
+# All death guard melee is within contagion range, hence all DG melee profiles have toughness_mod = -1
+
+dg_deathshroud_plaguespurt = {
+    'attacks_d6': 1,
+    'torrent': True,
+    'strength': 3,
+    'crit_wound_roll': 4,       # this is anti-infantry 4+, assuming attacking infantry
+    'ap': 0,
+    'ignores_cover': True,
+    'damage': 1,
+}
+dg_deathshroud_manreaper_strike = {
+    'attacks': 4,
+    'ballistic_skill': 2,
+    'lethal_hits': True,
+    'strength': 8,
+    'toughness_mod': -1,        # all death guard melee is within contagion range, hence I'm including this here
+    'ap': 2,
+    'ignores_cover': True,
+    'damage': 2,
+}
+dg_typhus_manreaper_strike = {
+    'attacks': 5,
+    'ballistic_skill': 2,
+    'lethal_hits': True,
+    'strength': 9,
+    'toughness_mod': -1,
+    'ap': 2,
+    'ignores_cover': True,
+    'damage': 3,
+}
+
+dg_plague_boltgun = {
+    'attacks': 2,
+    'ballistic_skill': 3,
+    'lethal_hits': True,
+    'strength': 4,
+    'ap': 0,
+    'damage': 1,
+}
+dg_plague_spewer = {
+    'attacks_d6': 1,
+    'torrent': True,
+    'strength': 5,
+    'crit_wound_roll': 2,
+    'ap': 1,
+    'ignores_cover': True,
+    'damage': 1,
+}
+dg_plague_belcher = {
+    'attacks_d6': 1,
+    'torrent': True,
+    'strength': 4,
+    'crit_wound_roll': 4,
+    'ap': 0,
+    'ignores_cover': True,
+    'damage': 1,
+}
+dg_blight_launcher = {
+    'attacks': 2,
+    'ballistic_skill': 3,
+    'lethal_hits': True,
+    'strength': 6,
+    'ap': 1,
+    'damage': 2,
+}
+dg_plague_knives = {
+    'attacks': 3,
+    'ballistic_skill': 3,
+    'lethal_hits': True,
+    'strength': 4,
+    'toughness_mod': -1,        # all death guard melee is within contagion range
+    'ap': 0,
+    'ignores_cover': True,
+    'damage': 1,
+}
+dg_bubotic_weapons = {
+    'attacks': 4,
+    'ballistic_skill': 3,
+    'lethal_hits': True,
+    'strength': 5,
+    'toughness_mod': -1,
+    'ap': 2,
+    'ignores_cover': True,
+    'damage': 1,
+}
+dg_heavy_plague_weapons = {
+    'attacks': 3,
+    'ballistic_skill': 4,
+    'lethal_hits': True,
+    'strength': 8,
+    'toughness_mod': -1,
+    'ap': 2,
+    'ignores_cover': True,
+    'damage': 2,
+}
+dg_tallyman_close_combat = {
+    'attacks': 4,
+    'ballistic_skill': 2,
+    'lethal_hits': False,
+    'strength': 4,
+    'toughness_mod': -1,
+    'ap': 0,
+    'ignores_cover': True,
+    'damage': 1,
+}
+dg_biologis_putrifier = {
+    'attacks': 4,
+    'ballistic_skill': 3,
+    'lethal_hits': True,        # biologis putrifier gives crits on 5+ to the unit he leads
+    'crit_hit_roll': 5,
+    'strength': 4,
+    'toughness_mod': -1,
+    'ap': 0,
+    'ignores_cover': True,
+    'damage': 1,
+}
+
+
+
+### Astra Militarum ###
+
+guard_lasgun = {
+    'attacks': 1,               # use 'attacks_mod': 1, to account for rapid fire
+    'ballistic_skill': 4,
+    # 'lethal_hits': False,
+    'strength': 3,
+    'ap': 0,
+    'damage': 1,
+}
+guard_grenade_krak = {
+    'attacks': 1,
+    'ballistic_skill': 4,
+    'strength': 9,
+    'ap': 2,
+    'damage_d3': 1,
+}
+guard_plasma_gun_standard = {
+    'attacks': 1,               # use 'attacks_mod': 1, to account for rapid fire
+    'ballistic_skill': 4,
+    'strength': 7,
+    'ap': 2,
+    'damage': 1,
+}
+guard_plasma_gun_supercharge = {
+    'attacks': 1,               # use 'attacks_mod': 1, to account for rapid fire
+    'ballistic_skill': 4,
+    'strength': 8,
+    'ap': 3,
+    'damage': 2,
+}
+guard_plasma_pistol_standard = {
+    'attacks': 1,
+    'ballistic_skill': 4,
+    'strength': 7,
+    'ap': 2,
+    'damage': 1,
+}
+guard_plasma_pistol_supercharge = {
+    'attacks': 1,
+    'ballistic_skill': 4,
+    'strength': 8,
+    'ap': 3,
+    'damage': 2,
+}
+guard_meltagun = {
+    'attacks': 1,
+    'ballistic_skill': 4,
+    'strength': 9,
+    'ap': 4,
+    'damage_d6': 1,             # use 'damage_mod': 2 to account for extra damage from melta distance
+}
+guard_lascannon = {
+    'attacks': 1,
+    'ballistic_skill': 5,
+    'strength': 12,
+    'ap': 3,
+    'damage': 1,
+    'damage_d6': 1,
+}
+guard_autocannon = {
+    'attacks': 2,
+    'ballistic_skill': 5,
+    'strength': 9,
+    'ap': 1,
+    'damage': 3,
+}
+guard_heavy_bolter = {
+    'attacks': 3,
+    'ballistic_skill': 5,
+    'sustained_hits': 1,
+    'strength': 5,
+    'ap': 1,
+    'damage': 2,
+}
+
+
+
+### Eldar ###
+
+eldar_wraithcannon = {
+    'attacks': 1,
+    'ballistic_skill': 4,
+    'strength': 14,
+    'ap': 4,
+    'damage_d6': 1,
+    'dev_wounds': True,
+}
+
+
+
+
+
+
+##### TARGET PROFILES #####
+
+target_profile_land_raider = {
+    'toughness': 12,
+    'armour_save': 2,
+    'unit_wounds_profile': [16],
+}
+target_profile_rhino = {
+    'toughness': 9,
+    'armour_save': 3,
+    'unit_wounds_profile': [10],
+}
+target_profile_custodes_guard = {
+    'toughness': 6,
+    'armour_save': 2,
+    'invuln_save': 4,
+    'feel_no_pain': 7,
+    'unit_wounds_profile': [4, 4, 3, 3, 6],     # assumes 2 shields, 2 spears, 1 leader
+}
+target_profile_termies = {
+    'toughness': 5,
+    'armour_save': 2,
+    'invuln_save': 4,
+    'unit_wounds_profile': [3]*5,
+}
+target_profile_space_marines = {
+    'toughness': 4,
+    'armour_save': 3,
+    'unit_wounds_profile': [2]*10,
+}
+target_profile_krieg = {            # whole profile assumes optimal defensive options
+    'toughness': 3,
+    'armour_save': 4,
+    'cover': True,
+    'feel_no_pain': 6,
+    'unit_wounds_profile': [1]*25,     
+}
+target_profile_necron_warriors = {
+    'toughness': 4,
+    'armour_save': 4,
+    'unit_wounds_profile': [1]*20,
+}
+target_profile_tau_crisis = {
+    'toughness':5,
+    'armour_save': 3,
+    'unit_wounds_profile': [5, 5, 4]
+}
+target_profile_wraithguard = {
+    'toughness': 7,
+    'armour_save': 2,
+    'feel_no_pain': 6,
+    'unit_wounds_profile': [3]*10
+}
